28b2d95504d3b9dd4858f0ae6c7fad5791a78823
A. Sivault <81822795+Sivarth91@users.noreply.github.com>
2022-07-14 12:59:42 +0200

    create plankton render

--- src/main/java/fr/sivarth/sbm/entity/render/PlanktonRender.java
+++ src/main/java/fr/sivarth/sbm/entity/render/PlanktonRender.java
@@ -1,4 +1,26 @@
 package fr.sivarth.sbm.entity.render;
 
-public class PlanktonRender {
+import fr.sivarth.sbm.SBM;
+import fr.sivarth.sbm.entity.custom.PlanktonEntity;
+import fr.sivarth.sbm.entity.model.PlanktonModel;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.util.ResourceLocation;
+
+public class PlanktonRender extends MobRenderer<PlanktonEntity, PlanktonModel<PlanktonEntity>> {
+
+    protected static final ResourceLocation TEXTURE = new ResourceLocation(SBM.MODID, "textures/entity/plankton.png");
+
+    /**
+     *
+     * @param rendererManagerIn
+     */
+    public PlanktonRender(EntityRendererManager rendererManagerIn) {
+        super(rendererManagerIn, new PlanktonModel<>(), 0.1f);
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(PlanktonEntity entity) {
+        return TEXTURE;
+    }
 }
