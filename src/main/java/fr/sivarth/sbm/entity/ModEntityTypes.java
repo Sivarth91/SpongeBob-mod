d42f56c535d7b95a697cac769d959c9e83d55c35
A. Sivault <81822795+Sivarth91@users.noreply.github.com>
2022-07-14 11:14:45 +0200

    entity types and registering

---
+++ src/main/java/fr/sivarth/sbm/entity/ModEntityTypes.java
@@ -0,0 +1,50 @@
+package fr.sivarth.sbm.entity;
+
+import fr.sivarth.sbm.SBM;
+import fr.sivarth.sbm.entity.custom.GaryEntity;
+import fr.sivarth.sbm.entity.custom.PatrickEntity;
+import fr.sivarth.sbm.entity.custom.PlanktonEntity;
+import fr.sivarth.sbm.entity.custom.SpongeBobEntity;
+import net.minecraft.client.renderer.entity.model.ZombieModel;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModEntityTypes {
+
+    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, SBM.MODID);
+
+    /**
+     *
+     * @param bus
+     */
+    public static void register(IEventBus bus) {
+        ENTITY_TYPES.register(bus);
+    }
+
+
+    public static final RegistryObject<EntityType<SpongeBobEntity>> SPONGE_BOB = ENTITY_TYPES.register("sponge_bob",
+            () -> EntityType.Builder.of(SpongeBobEntity::new, EntityClassification.CREATURE)
+                    .sized(1.0f, 2.0f)
+                    .build(new ResourceLocation(SBM.MODID, "sponge_bob").toString()));
+
+    public static final RegistryObject<EntityType<PatrickEntity>> PATRICK = ENTITY_TYPES.register("patrick",
+        () -> EntityType.Builder.of(PatrickEntity::new, EntityClassification.CREATURE)
+            .sized(1.0f, 2.2f)
+            .build(new ResourceLocation(SBM.MODID, "patrick").toString()));
+
+    public static final RegistryObject<EntityType<GaryEntity>> GARY = ENTITY_TYPES.register("gary",
+        () -> EntityType.Builder.of(GaryEntity::new, EntityClassification.CREATURE)
+            .sized(1.0f, 1.0f)
+            .build(new ResourceLocation(SBM.MODID, "gary").toString()));
+
+    public static final RegistryObject<EntityType<PlanktonEntity>> PLANKTON = ENTITY_TYPES.register("plankton",
+        () -> EntityType.Builder.of(PlanktonEntity::new, EntityClassification.MONSTER)
+            .sized(1.0f, 1.0f)
+            .build(new ResourceLocation(SBM.MODID, "plankton").toString()));
+
+}
---
+++ src/main/java/fr/sivarth/sbm/utils/ClientEventBusSubscriber.java
@@ -0,0 +1,46 @@
+package fr.sivarth.sbm.utils;
+
+import fr.sivarth.sbm.SBM;
+import fr.sivarth.sbm.entity.ModEntityTypes;
+import fr.sivarth.sbm.entity.render.GaryRender;
+import fr.sivarth.sbm.entity.render.PatrickRender;
+import fr.sivarth.sbm.entity.render.PlanktonRender;
+import fr.sivarth.sbm.entity.render.SpongeBobRender;
+import fr.sivarth.sbm.item.custom.ModSpawnEggItem;
+import net.minecraft.entity.EntityType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = SBM.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientEventBusSubscriber {
+
+    /**
+     *
+     * @param e
+     */
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent e) {
+        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.SPONGE_BOB.get(), SpongeBobRender::new);
+        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.PATRICK.get(), PatrickRender::new);
+        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.GARY.get(), GaryRender::new);
+        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.PLANKTON.get(), PlanktonRender::new);
+    }
+
+    /**
+     *
+     * @param event
+     */
+    @SubscribeEvent
+    public static void onRegisterEntities(final RegistryEvent.Register<EntityType<?>> event) {
+        ModSpawnEggItem.initSpawnEggs();
+    }
+
+
+
+
+
+}
