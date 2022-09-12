900291e6ff0b2e78ac34de506aeef3a55fd7700b
Sivault-Arthur <80334990+Sivault-Arthur@users.noreply.github.com>
2022-09-12 18:11:17 +0200

    update plankton model and texture

--- src/main/java/fr/sivarth/sbm/entity/model/PlanktonModel.java
+++ src/main/java/fr/sivarth/sbm/entity/model/PlanktonModel.java
@@ -8,15 +8,10 @@ import net.minecraft.entity.Entity;
 
 public class PlanktonModel<T extends Entity> extends EntityModel<T> {
 
-
     private final ModelRenderer body;
     private final ModelRenderer legs;
-    private final ModelRenderer rightLeg;
-    private final ModelRenderer leftLeg;
-    private final ModelRenderer amrs;
-    private final ModelRenderer rightArm;
+    private final ModelRenderer arms;
     private final ModelRenderer cube_r1;
-    private final ModelRenderer leftArm;
     private final ModelRenderer cube_r2;
 
     public PlanktonModel() {
@@ -24,53 +19,35 @@ public class PlanktonModel<T extends Entity> extends EntityModel<T> {
         texHeight = 32;
 
         body = new ModelRenderer(this);
-        body.setPos(0.0F, 24.0F, 0.0F);
-        body.texOffs(0, 0).addBox(0.0F, -10.0F, -1.0F, 5.0F, 7.0F, 3.0F, 0.0F, false);
-        body.texOffs(4, 10).addBox(1.0F, -14.0F, 0.0F, 1.0F, 4.0F, 1.0F, 0.0F, false);
-        body.texOffs(0, 10).addBox(3.0F, -14.0F, 0.0F, 1.0F, 4.0F, 1.0F, 0.0F, false);
-        body.texOffs(15, 15).addBox(2.0F, -12.0F, 0.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-        body.texOffs(15, 13).addBox(0.0F, -12.0F, 0.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-        body.texOffs(15, 9).addBox(4.0F, -12.0F, 0.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        body.setPos(0.0F, 23.0F, 0.0F);
+        body.texOffs(0, 0).addBox(-4.0F, -15.0F, -2.0F, 7.0F, 12.0F, 3.0F, 0.0F, false);
+        body.texOffs(0, 20).addBox(-3.0F, -18.0F, -1.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
+        body.texOffs(19, 18).addBox(1.0F, -18.0F, -1.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
+        body.texOffs(15, 19).addBox(2.0F, -20.0F, -1.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
+        body.texOffs(16, 15).addBox(-4.0F, -20.0F, -1.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
 
         legs = new ModelRenderer(this);
-        legs.setPos(0.0F, 24.0F, 0.0F);
+        legs.setPos(0.0F, 1.0F, 0.0F);
+        body.addChild(legs);
+        legs.texOffs(12, 15).addBox(-3.0F, -4.0F, -1.0F, 1.0F, 4.0F, 1.0F, 0.0F, false);
+        legs.texOffs(8, 15).addBox(1.0F, -4.0F, -1.0F, 1.0F, 4.0F, 1.0F, 0.0F, false);
 
+        arms = new ModelRenderer(this);
+        arms.setPos(0.0F, 1.0F, 0.0F);
+        body.addChild(arms);
 
-        rightLeg = new ModelRenderer(this);
-        rightLeg.setPos(0.0F, 0.0F, 0.0F);
-        legs.addChild(rightLeg);
-        rightLeg.texOffs(11, 14).addBox(3.0F, -3.0F, 0.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
-
-        leftLeg = new ModelRenderer(this);
-        leftLeg.setPos(0.0F, 0.0F, 0.0F);
-        legs.addChild(leftLeg);
-        leftLeg.texOffs(7, 14).addBox(1.0F, -3.0F, 0.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
-
-        amrs = new ModelRenderer(this);
-        amrs.setPos(0.0F, 24.0F, 0.0F);
-
-
-        rightArm = new ModelRenderer(this);
-        rightArm.setPos(0.0F, 0.0F, 0.0F);
-        amrs.addChild(rightArm);
-        rightArm.texOffs(0, 15).addBox(5.0F, -8.0F, 0.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
 
         cube_r1 = new ModelRenderer(this);
-        cube_r1.setPos(5.0F, 0.0F, 0.0F);
-        rightArm.addChild(cube_r1);
-        setRotationAngle(cube_r1, 0.0F, 0.0F, -0.1309F);
-        cube_r1.texOffs(12, 10).addBox(1.0F, -8.0F, 0.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
-
-        leftArm = new ModelRenderer(this);
-        leftArm.setPos(0.0F, 0.0F, 0.0F);
-        amrs.addChild(leftArm);
-        leftArm.texOffs(13, 0).addBox(-1.0F, -8.0F, 0.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        cube_r1.setPos(0.0F, 0.0F, 0.0F);
+        arms.addChild(cube_r1);
+        setRotationAngle(cube_r1, 0.0F, 0.0F, 0.1134F);
+        cube_r1.texOffs(0, 15).addBox(-6.0F, -9.0F, -1.0F, 1.0F, 4.0F, 1.0F, 0.0F, false);
 
         cube_r2 = new ModelRenderer(this);
         cube_r2.setPos(0.0F, 0.0F, 0.0F);
-        leftArm.addChild(cube_r2);
-        setRotationAngle(cube_r2, 0.0F, 0.0F, 0.1309F);
-        cube_r2.texOffs(8, 10).addBox(-2.0F, -8.0F, 0.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
+        arms.addChild(cube_r2);
+        setRotationAngle(cube_r2, 0.0F, 0.0F, -0.1134F);
+        cube_r2.texOffs(4, 15).addBox(4.0F, -9.0F, -1.0F, 1.0F, 4.0F, 1.0F, 0.0F, false);
     }
 
     @Override
@@ -81,8 +58,6 @@ public class PlanktonModel<T extends Entity> extends EntityModel<T> {
     @Override
     public void renderToBuffer(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
         body.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
-        legs.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
-        amrs.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
     }
 
     public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
