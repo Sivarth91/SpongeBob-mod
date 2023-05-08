6a87d0d1b5d85969e1affa30b8573ba8df3ba533
Sivarth91 <sivaultarthur@gmail.com>
2023-05-08 18:29:42 +0200

    feat: add some blocks

--- src/main/java/fr/sivarth/sbm/block/ModBlocks.java
+++ src/main/java/fr/sivarth/sbm/block/ModBlocks.java
@@ -2,6 +2,7 @@ package fr.sivarth.sbm.block;
 
 import fr.sivarth.sbm.SBM;
 import fr.sivarth.sbm.block.custom.*;
+import fr.sivarth.sbm.block.custom.trees.PalmTree;
 import fr.sivarth.sbm.item.ModItemGroup;
 import fr.sivarth.sbm.item.ModItems;
 import net.minecraft.block.*;
@@ -20,7 +21,7 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, SBM.MODID);
 
     public static final RegistryObject<Block> PINEAPPLE_BLOCK = registerBlock("pineapple_block", () -> new Block(AbstractBlock.Properties.of(Material.PLANT).strength(1.5f, 6).harvestTool(ToolType.SHOVEL).harvestLevel(0).sound(SoundType.SHROOMLIGHT)));
-    public static final RegistryObject<Block> PINEAPPLE_LEAVES = registerBlock("pineapple_leaves", () -> new Block(AbstractBlock.Properties.of(Material.WOOD).strength(1, 2).sound(SoundType.LILY_PAD).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> PINEAPPLE_LEAVES = registerBlock("pineapple_leaves", () -> new LeavesBlock(AbstractBlock.Properties.of(Material.LEAVES).strength(0.2f, 0.2f).sound(SoundType.GRASS).noOcclusion().requiresCorrectToolForDrops()));
     public static final RegistryObject<Block> TUBE = registerBlock("tube", () -> new Block(AbstractBlock.Properties.of(Material.STONE).strength(3, 5).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.METAL).requiresCorrectToolForDrops()));
     public static final RegistryObject<Block> SAND_ROAD = registerBlock("sand_road", () -> new Block(AbstractBlock.Properties.of(Material.SAND).strength(3.2f,2.1f).harvestTool(ToolType.SHOVEL).harvestLevel(0).sound(SoundType.SAND)));
     public static final RegistryObject<Block> ALUMINIUM_WALL_BLOCK = registerBlock("aluminium_wall_block", () -> new Block(AbstractBlock.Properties.of(Material.METAL).strength(4.2f, 3.7f).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.METAL).requiresCorrectToolForDrops()));
@@ -30,14 +31,15 @@ public class ModBlocks {
     public static final RegistryObject<Block> ONIONS = BLOCKS.register("onion_crops", () -> new OnionBlock(AbstractBlock.Properties.copy(Blocks.WHEAT)));
     public static final RegistryObject<Block> TOMATOES = BLOCKS.register("tomato_crops", () -> new TomatoesBlock(AbstractBlock.Properties.copy(Blocks.WHEAT)));
     public static final RegistryObject<Block> PATRICK_STONE = registerBlock("patrick_stone", () -> new Block(AbstractBlock.Properties.of(Material.STONE).strength(4.3f, 3.2f).harvestTool(ToolType.PICKAXE).harvestLevel(0).sound(SoundType.STONE).requiresCorrectToolForDrops()));
-    public static final RegistryObject<Block> CASH_REGISTER = registerBlock("cash_register", () -> new Block(AbstractBlock.Properties.of(Material.METAL)));
+    public static final RegistryObject<Block> SAS_BLOCK = registerBlock("sas_block", () -> new Block(AbstractBlock.Properties.of(Material.METAL).strength(4.0f, 3.5f).harvestTool(ToolType.PICKAXE).harvestLevel(2).sound(SoundType.METAL).requiresCorrectToolForDrops()));
 
-    public static final RegistryObject<Block> CORAL_PLANKS = registerBlock("coral_planks", () -> new Block(AbstractBlock.Properties.of(Material.WOOD).strength(3.5f, 2.9f).harvestTool(ToolType.HOE).harvestLevel(0).sound(SoundType.WOOD)));
-    public static final RegistryObject<Block> CORAL_PLANKS_STAIRS = registerBlock("coral_planks_stairs", () -> new StairsBlock(() -> CORAL_PLANKS.get().defaultBlockState(), AbstractBlock.Properties.of(Material.WOOD).strength(3.5f, 2.9f).harvestTool(ToolType.AXE).harvestLevel(0).sound(SoundType.WOOD)));
+    public static final RegistryObject<Block> CORAL_PLANKS = registerBlock("coral_planks", () -> new Block(AbstractBlock.Properties.of(Material.WOOD).strength(3.2f, 2.5f).harvestTool(ToolType.HOE).harvestLevel(0).sound(SoundType.WOOD)));
+    public static final RegistryObject<Block> CORAL_PLANKS_STAIRS = registerBlock("coral_planks_stairs", () -> new StairsBlock(() -> CORAL_PLANKS.get().defaultBlockState(), AbstractBlock.Properties.of(Material.WOOD).strength(3.2f, 2.5f).harvestTool(ToolType.AXE).harvestLevel(0).sound(SoundType.WOOD)));
     public static final RegistryObject<Block> CORAL_PLANKS_SLAB = registerBlock("coral_planks_slab", () -> new SlabBlock(AbstractBlock.Properties.of(Material.WOOD).strength(3.2f, 2.5f).harvestTool(ToolType.AXE).harvestLevel(0).sound(SoundType.WOOD)));
+    public static final RegistryObject<Block> CORAL_PLANKS_FENCE = registerBlock("coral_planks_fence", () -> new FenceBlock(AbstractBlock.Properties.of(Material.WOOD).strength(3.2f, 2.5f).harvestTool(ToolType.AXE).harvestLevel(0).sound(SoundType.WOOD)));
+    public static final RegistryObject<Block> CORAL_PLANKS_FENCE_GATE = registerBlock("coral_planks_fence_gate", () -> new FenceGateBlock(AbstractBlock.Properties.of(Material.WOOD).strength(3.2f, 2.5f).harvestTool(ToolType.AXE).harvestLevel(0).sound(SoundType.WOOD)));
 
-
-    public static final RegistryObject<Block> ROUND_WINDOW = registerBlock("round_window", () -> new RoundWindowBlock(AbstractBlock.Properties.of(Material.METAL).strength(4.1f, 3.5f).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.METAL).requiresCorrectToolForDrops().air()));
+    public static final RegistryObject<Block> ROUND_WINDOW = registerBlock("round_window", () -> new RoundWindowBlock(AbstractBlock.Properties.of(Material.METAL).strength(4.1f, 3.5f).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.METAL).noOcclusion().requiresCorrectToolForDrops()));
     public static final RegistryObject<Block> TELEVISION = registerBlock("television", () -> new TelevisionBlock(AbstractBlock.Properties.of(Material.METAL).strength(3.3f, 3.3f).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.GLASS).requiresCorrectToolForDrops().air()));
 
     public static final RegistryObject<Block> ALUMINIUM_DOOR = registerBlock("aluminium_door", () -> new DoorBlock(AbstractBlock.Properties.of(Material.WOOD).strength(4.5f, 3.7f).harvestTool(ToolType.PICKAXE).harvestLevel(1).sound(SoundType.METAL).requiresCorrectToolForDrops()));
@@ -45,6 +47,9 @@ public class ModBlocks {
     public static final RegistryObject<Block> REINFORCED_ALUMINIUM_DOOR = registerBlock("reinforced_aluminium_door", () -> new DoorBlock(AbstractBlock.Properties.of(Material.METAL).strength(5.2f, 4.4f).harvestTool(ToolType.PICKAXE).harvestLevel(2).sound(SoundType.METAL).requiresCorrectToolForDrops()));
     public static final RegistryObject<Block> REINFORCE_ALUMINIUM_TRAPDOOR = registerBlock("reinforced_aluminium_trapdoor", () -> new TrapDoorBlock(AbstractBlock.Properties.of(Material.METAL).strength(5.2f, 4.4f).harvestTool(ToolType.PICKAXE).harvestLevel(2).sound(SoundType.METAL).requiresCorrectToolForDrops()));
 
+    public static final RegistryObject<Block> PALM_LOG = registerBlock("palm_log", () -> new RotatedPillarBlock(AbstractBlock.Properties.of(Material.WOOD)));
+    public static final RegistryObject<Block> PALM_LEAVES = registerBlock("palm_leaves", () -> new LeavesBlock(AbstractBlock.Properties.of(Material.LEAVES).strength(0.2f, 0.2f).sound(SoundType.GRASS).noOcclusion().requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> PALM_SAPLING = registerBlock("palm_sapling",() -> new SaplingBlock(new PalmTree(), AbstractBlock.Properties.copy(Blocks.DEAD_BUSH)));
 
     /**
      *
